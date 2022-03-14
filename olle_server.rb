329d6a87979e587a43938c50ea83ea97f5c3dec3
Olle Ronstad <olle.ronstad@gmail.com>
2022-03-14 09:50:34 +0100

    add some things

--- olle_server.rb
+++ olle_server.rb
@@ -1,37 +1,70 @@
 require 'socket'
+require_relative 'colorize.rb'
 
-def parse_http(text)
-    out = {}
-    out[:type] = text.match(/\w+/).to_s
-    out[:path] = text.match(/(?<=\/)[\w.\/]+/).to_s
-    out[:protocol] = text.split(" ")[2]
-    text.split("\n")[1..-1].each do |row|
-        key, val = row.split(": ")
-        out[key.gsub("-","_").to_sym] = val
-    end
-    out
-end
 
 class Olle_server
 
+    def start()
+        puts "\nStarted server\n".green
+        loop do
+            session = @server.accept
+            puts "\n\nThe socket is: "+ session.to_s.yellow
+            
+            request = ""
+            while (line = session.gets) and line !~ /^\s*$/
+                request += line
+            end
+            request = parse_http(request)
+            puts_request(request)
+
+            send_response(request)
+            
+            session.close
+        end
+        
+    end
+    
+    private
+
+    #puts the hash wich contains the data to the terminal with colors
+    def puts_request(request)
+        request.each do |key,value|
+            puts key.to_s.blue + " => " + value.red
+        end
+    end
+        
+    #parse a http requst to an hash
+    def parse_http(text)
+        out = {}
+        out[:type] = text.split(" ")[0]
+        out[:path] = text.split(" ")[1]
+        out[:protocol] = text.split(" ")[2]
+        text.split("\n")[1..-1].each do |row|
+            key, val = row.split(": ")
+            out[key.gsub("-","_").to_sym] = val
+        end
+        out
+    end
+    
+    def gen_http_response(body)
+        response = "HTTP/1.1 200 OK\nServer: Olle_server\n\n#{body}\n\n"
+    end
+    
     #initilazes the Olle_server object by creating a TCP server with the specified port
     def initialize(port)
         @server = TCPServer.new(port)
         @get_routes = {}
         @post_routes = {}
     end
-
+    
     #add the provided block of code to the specified Get PATH
-    def GET(path_sym, &block)
-        @get_routes[path_sym] = block
+    def GET(path, &block)
+        @get_routes[path] = block
     end
     
     #add the provided block of code to the specified post PATH
-    def POST(path_sym, &block)
-        @post_routes[path_sym] = block
+    def POST(path, &block)
+        @post_routes[path] = block
     end
 
-
-    def start()
-        
 end
