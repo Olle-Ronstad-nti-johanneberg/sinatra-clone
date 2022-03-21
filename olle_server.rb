79d1f697d3cfa4eca9f0bf8ba71eb60e7dd389af
Olle Ronstad <olle.ronstad@gmail.com>
2022-03-21 09:49:22 +0100

    add slim method, send_respons method. initilaze.

--- olle_server.rb
+++ olle_server.rb
@@ -1,15 +1,42 @@
 require 'socket'
+require 'slim'
 require_relative 'colorize.rb'
+
+def slim(file_name)
+    layout = File.open("views/layout.slim", "rb").read
+    contents = File.open("views/#{file_name}.slim", "rb").read
+
+    l = Slim::Template.new{layout}
+    c = Slim::Template.new{contents}.render()
+    l.render{c}
+end
+
 
 
 class Olle_server
+    
+    #initilazes the Olle_server object by creating a TCP server with the specified port
+    def initialize(port)
+        @server = TCPServer.new(port)
+        @get_routes = {}
+        @post_routes = {}
+    end
+    
+    #add the provided block of code to the specified Get PATH
+    def GET(path, &block)
+        @get_routes[path] = block
+    end
+    
+    #add the provided block of code to the specified post PATH
+    def POST(path, &block)
+        @post_routes[path] = block
+    end
 
     def start()
         puts "\nStarted server\n".green
         loop do
             session = @server.accept
             puts "\n\nThe socket is: "+ session.to_s.yellow
-            
             request = ""
             while (line = session.gets) and line !~ /^\s*$/
                 request += line
@@ -17,8 +44,8 @@ class Olle_server
             request = parse_http(request)
             puts_request(request)
 
-            send_response(request)
-            
+            send_response(session,request)
+            puts "Closing socket: " + session.to_s.yellow
             session.close
         end
         
@@ -50,21 +77,30 @@ class Olle_server
         response = "HTTP/1.1 200 OK\nServer: Olle_server\n\n#{body}\n\n"
     end
     
-    #initilazes the Olle_server object by creating a TCP server with the specified port
-    def initialize(port)
-        @server = TCPServer.new(port)
-        @get_routes = {}
-        @post_routes = {}
+    def send_response(socket, request)
+        case request[:type]
+        when "GET"
+            if @get_routes[request[:path]] != nil
+                socket.puts(
+                    gen_http_response(
+                        @get_routes[request[:path]].call(request)
+                        )
+                    )
+            else
+                socket.puts("HTTP/1.1 404\nServer: Olle_server")
+            end
+        when "POST"
+            if @post_routes[request[:path]] != nil
+                socket.puts(
+                    gen_http_response(
+                        @post_routes[request[:path]].call(request)
+                        )
+                    )
+            else
+                socket.puts("HTTP/1.1 404\nServer: Olle_server")
+            end
+        else
+            socket.puts("HTTP/1.1 500\nServer: Olle_server")
+        end
     end
-    
-    #add the provided block of code to the specified Get PATH
-    def GET(path, &block)
-        @get_routes[path] = block
-    end
-    
-    #add the provided block of code to the specified post PATH
-    def POST(path, &block)
-        @post_routes[path] = block
-    end
-
 end
