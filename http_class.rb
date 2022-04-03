26cff165bcc3136eeab35e755157a7ba2f679414
Olle Ronstad <olle.ronstad@gmail.com>
2022-04-03 14:49:28 +0200

    add image response class

--- http_class.rb
+++ http_class.rb
@@ -81,3 +81,35 @@ class Http_response
         puts self.to_s.green
     end
 end
+
+
+
+class HTTP_response_image
+    def initialize(file)
+        @protocol ="HTTP/1.1"
+        @status_code = "200 OK"
+        @body = File.open("public#{file}", "rb").read
+        @header = {
+            "Content-Type" => "image/#{file.split(".")[-1]}; charset=utf-8",
+            "Server" => "olle_server"
+        }
+    end
+
+    def to_s
+        "#{@protocol} #{@status_code}\r\n#{
+            @header.to_a.map do |key,value|
+                key + ": " + value
+            end.join("\r\n")
+
+        }\n\n#{@body}\n\n"
+    end
+
+    def print
+        puts "#{@protocol} #{@status_code}\r\n#{
+            @header.to_a.map do |key,value|
+                key + ": " + value
+            end.join("\r\n")
+
+        }\n\nimage".green
+    end
+end
