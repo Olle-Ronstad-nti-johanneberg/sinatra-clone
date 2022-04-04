1204003d325f4c07c5ea9f56965a2c2ad561cd80
Olle Ronstad <olle.ronstad@gmail.com>
2022-04-04 09:49:16 +0200

    remove puts

--- adv_gets.rb
+++ adv_gets.rb
@@ -2,14 +2,11 @@ def adv_gets(socket)
     body_length = 0
     request = ""
     while line = socket.gets and line !~ /^\s*$/
-        p line
         if line[0..15] == "Content-Length: "
             body_length = line[16..-1].to_i
         end
         request += line
     end
-    p "read body"
     body = socket.read(body_length)
-    p body
     return request, body
 end
