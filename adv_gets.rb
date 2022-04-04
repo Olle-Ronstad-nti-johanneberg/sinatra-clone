77b85e904f81430b7d3d566ad697ebc013c16e86
Olle Ronstad <olle.ronstad@gmail.com>
2022-04-04 09:41:37 +0200

    refactor adv_gets

--- adv_gets.rb
+++ adv_gets.rb
@@ -1,8 +1,15 @@
-def adv_gets(cli)
-    msg = ""
-    sleep (0.001)
-    while cli.ready?
-        msg += cli.getc
+def adv_gets(socket)
+    body_length = 0
+    request = ""
+    while line = socket.gets and line !~ /^\s*$/
+        p line
+        if line[0..15] == "Content-Length: "
+            body_length = line[16..-1].to_i
+        end
+        request += line
     end
-    msg
+    p "read body"
+    body = socket.read(body_length)
+    p body
+    return request, body
 end
