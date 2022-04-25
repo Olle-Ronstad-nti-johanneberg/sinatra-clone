8ee65f997bd40281692813e9d31e05da096b768d
Olle Ronstad <olle.ronstad@gmail.com>
2022-04-25 09:26:57 +0200

    clean code

--- colorize.rb
+++ colorize.rb
@@ -1,23 +1,29 @@
-class String #detta finns för att färglägaw text med asni code
-    def colorize(color_code)
-      "\e[#{color_code}m#{self}\e[0m"
-    end
-    def red
-      colorize(31)
-    end
-    def green
-      colorize(32)
-    end
-    def yellow
-      colorize(33)
-    end
-    def blue
-      colorize(34)
-    end
-    def pink
-      colorize(35)
-    end
-    def light_blue
-      colorize(36)
-    end
+class String # detta finns för att färglägaw text med asni code
+  def colorize(color_code)
+    "\e[#{color_code}m#{self}\e[0m"
+  end
+
+  def red
+    colorize(31)
+  end
+
+  def green
+    colorize(32)
+  end
+
+  def yellow
+    colorize(33)
+  end
+
+  def blue
+    colorize(34)
+  end
+
+  def pink
+    colorize(35)
+  end
+
+  def light_blue
+    colorize(36)
+  end
 end
