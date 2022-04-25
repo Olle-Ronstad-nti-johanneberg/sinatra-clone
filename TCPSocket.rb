ea2a06d35cb1e1ca797cf2d029db888d4ff5be4a
Olle Ronstad <olle.ronstad@gmail.com>
2022-04-25 13:47:50 +0200

    refactor content legnht

--- TCPSocket.rb
+++ TCPSocket.rb
@@ -1,11 +1,18 @@
 class TCPSocket
   def get_http
-    body_length = 0
     request = ''
     while line = self.gets and line !~ /^\s*$/
-      body_length = line[16..-1].to_i if line[0..15] == 'Content-Length: '
       request += line
     end
+    content_length_line = request.split("\n").select do |line|
+      line.include?('Content-Length:')
+    end.first
+    body_length = if content_length_line.nil?
+                    0
+                  else
+                    content_length_line[16..-1].to_i
+                  end
+
     body = read(body_length)
     [request, body]
   end
