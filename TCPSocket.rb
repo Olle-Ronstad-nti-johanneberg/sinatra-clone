e9c55830a3c084214a83d7eca1b83695ff4000ca
Olle Ronstad <olle.ronstad@gmail.com>
2022-04-25 09:45:32 +0200

    clean code

--- TCPSocket.rb
+++ TCPSocket.rb
@@ -1,14 +1,12 @@
 class TCPSocket
-    def get_http
-        body_length = 0
-        request = ""
-        while line = self.gets and line !~ /^\s*$/
-            if line[0..15] == "Content-Length: "
-                body_length = line[16..-1].to_i
-            end
-            request += line
-        end
-        body = self.read(body_length)
-        return request, body
+  def get_http
+    body_length = 0
+    request = ''
+    while line = self.gets and line !~ /^\s*$/
+      body_length = line[16..-1].to_i if line[0..15] == 'Content-Length: '
+      request += line
     end
+    body = read(body_length)
+    [request, body]
+  end
 end
